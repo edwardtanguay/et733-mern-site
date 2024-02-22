67ae4c98b64d123bf34840b684883b8a2ff7a96e
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-22 02:43:20 +0100

    showing 9 employees

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import { IEmployee } from "../interfaces";
+import axios from "axios";
+
+const backendUrl = "http://localhost:4206";
+
 export const PageWelcome = () => {
+	const [employees, setEmployees] = useState<IEmployee[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${backendUrl}/employees`);
+			const _employees = response.data;
+			setEmployees(_employees);
+		})();
+	}, []);
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<h2>There are {employees.length} employees:</h2>
+		</>
+	);
+};
